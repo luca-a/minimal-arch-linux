a44441b74a8be79d6864b3bb010a71db4970685e
Luca <38193617+luca-a@users.noreply.github.com>
2019-08-22 13:09:41 +0200

    Edit post install script
    Removed unused packages and replaced old links pointing to initial repository

--- 2_arch_post_install.sh
+++ 2_arch_post_install.sh
@@ -11,47 +11,20 @@ sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-echo "Installing and enabling TLP"
-sudo pacman -S tlp tlp-rdw
-sudo systemctl enable tlp.service
-sudo systemctl enable tlp.service
-sudo systemctl start tlp.service
-sudo systemctl start tlp.service
-sudo systemctl enable NetworkManager-dispatcher.service
-sudo systemctl mask systemd-rfkill.service
-sudo systemctl mask systemd-rfkill.socket
-
 echo "Installing common applications"
-echo -en "1\nyes" | sudo pacman -S firefox keepassxc git openssh vim links alacritty upower htop powertop
-
-echo "Installing office applications"
-yes | sudo pacman -S tumbler evince gimp inkscape thunderbird
+echo -en "1\nyes" | sudo pacman -S git openssh links alacritty upower htop powertop
 
 echo "Installing fonts"
 yes | sudo pacman -S ttf-droid ttf-opensans ttf-dejavu ttf-liberation ttf-hack ttf-fira-code
 
-echo "Installing VS Code, NVM and Node.js LTS"
-yes | sudo pacman -S code
-git clone https://aur.archlinux.org/nvm.git
-cd nvm
-yes | makepkg -si
-cd ..
-rm -rf nvm
-source /usr/share/nvm/init-nvm.sh
-nvm install --lts=dubnium
-
-echo "Installing VS Code theme and icons"
-code --install-extension jolaleye.horizon-theme-vscode
-code --install-extension vscode-icons-team.vscode-icons
-
 echo "Installing and setting zsh"
 yes | sudo pacman -S zsh zsh-theme-powerlevel9k
 chsh -s /bin/zsh
-wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zshrc
-wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zprofile
+wget -P ~/ https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/zsh/.zshrc
+wget -P ~/ https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/zsh/.zprofile
 mkdir -p ~/.zshrc.d
-wget -P ~/.zshrc.d https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zshrc.d/environ.zsh
-wget -P ~/.zshrc.d https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zshrc.d/wayland.zsh
+wget -P ~/.zshrc.d https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/zsh/.zshrc.d/environ.zsh
+wget -P ~/.zshrc.d https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/zsh/.zshrc.d/wayland.zsh
 
 echo "Setting up GTK theme and installing dependencies"
 yes | sudo pacman -S gtk-engine-murrine gtk-engines
@@ -77,37 +50,22 @@ sudo wget -P /usr/share/fonts/TTF/ https://raw.githubusercontent.com/Templarian/
 echo "Installing sway and additional packages"
 yes | sudo pacman -S sway swaylock swayidle waybar pulseaudio pavucontrol rofi slurp grim thunar mousepad nnn light feh qalculate-gtk
 mkdir -p ~/.config/sway
-wget -P ~/.config/sway/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/sway/config
+wget -P ~/.config/sway/ https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/sway/config
 mkdir -p ~/Pictures/screenshots
 
-echo "Enabling auto-mount for thunar"
-yes | sudo pacman -S gvfs thunar-volman
-
 echo "Setting wallpaper"
-wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpaper/6303-mauritania.jpg
+wget -P ~/Pictures/ https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/wallpaper/6303-mauritania.jpg
 
 echo "Ricing waybar"
 mkdir -p ~/.config/waybar
-wget -P ~/.config/waybar https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/waybar/config
-wget -P ~/.config/waybar https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/waybar/style.css
+wget -P ~/.config/waybar https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/waybar/config
+wget -P ~/.config/waybar https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/waybar/style.css
 
 echo "Ricing Alacritty"
 mkdir -p ~/.config/alacritty
-wget -P ~/.config/alacritty https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/alacritty/alacritty.yml
-
-echo "Blacklisting bluetooth"
-sudo touch /etc/modprobe.d/nobt.conf
-sudo tee /etc/modprobe.d/nobt.conf << END
-blacklist btusb
-blacklist bluetooth
-END
-sudo mkinitcpio -p linux
+wget -P ~/.config/alacritty https://raw.githubusercontent.com/luca-a/minimal-arch-linux/master/configs/alacritty/alacritty.yml
 
 echo "Increasing the amount of inotify watchers"
 echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
-
-echo "Enabling audio power saving"
-sudo touch /etc/modprobe.d/audio_powersave.conf
-echo "options snd_hda_intel power_save=1" | sudo tee /etc/modprobe.d/audio_powersave.conf
 
 echo "Your setup is ready. You can reboot now!"
