fd7b294a8476ea54f36ea7dad65398391cef6072
Luca <38193617+luca-a@users.noreply.github.com>
2019-10-11 22:14:14 +0200

    Update 2_base.sh
    Removed sublime installation

--- 2_base.sh
+++ 2_base.sh
@@ -33,28 +33,6 @@ sudo echo 6 > /sys/kernel/debug/bluetooth/hci0/conn_min_interval
 echo "Installing common applications"
 echo -en "1\nyes" | sudo pacman -S chromium git openssh links upower htop
 
-echo "Installing and setting sublime text"
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-yes | sudo pacman -Syu sublime-text
-sudo touch ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-sudo tee ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings << END
-{
-	"font_size": 10,
-	"theme": "Adaptive.sublime-theme"
-}
-END
-sudo touch ~/.config/sublime-text-3/Packages/User/Package Control.sublime-settings
-sudo tee ~/.config/sublime-text-3/Packages/User/Package Control.sublime-settings << END
-{
-	"installed_packages":
-	[
-		"Git Conflict Resolver",
-		"Package Control"
-	]
-}
-END
-
 echo "Installing fonts"
 yes | sudo pacman -S ttf-droid ttf-opensans ttf-dejavu ttf-liberation ttf-hack ttf-fira-code noto-fonts gsfonts powerline-fonts
 
